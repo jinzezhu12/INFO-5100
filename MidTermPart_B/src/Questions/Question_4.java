dffdd9d86355aad967562ca8f0163dd941819e85
Jin Zezhu <jinzezhu@JindeMacBook-Pro.local>
2017-10-26 20:13:08 -0700

    question_4 completed

--- MidTermPart_B/src/Questions/Question_4.java
+++ MidTermPart_B/src/Questions/Question_4.java
@@ -12,21 +12,20 @@ public class Question_4 {
     }
 
     public static int countNumberOfPossibleWays(int m, int n, int x){
-        int[][] ways = new int[n + 1][x + 1];
-        int min = Math.min(x,m);
+        int[][] ways = new int[m + 1][x + 1];
+        int min = Math.min(x,n);
         for(int i = 1; i<= min; i++) {
             ways[1][i] = 1;
         }
 
-        for (int i  = 2; i <= n ;i++) {
+        for (int i  = 2; i <= m ;i++) {
             for (int j  = 1; j <= x ; j++)
-                for (int k = 1; k <= m && k < j; k++)
+                for (int k = 1; k <= n && k < j; k++)
                     ways[i][j] += ways[i-1][j - k];
 
         }
-        return ways[n][x];
+        return ways[m][x];
     }
-
 
 }
 
